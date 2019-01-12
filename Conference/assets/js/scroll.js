1d70e4b4285114b58adbeb8fbb6fbf2e3c15eddc
sainathomdas <43498316+sainathomdas@users.noreply.github.com>
2019-01-13 00:28:49 +0530

    scroll.js updated
    new feature : nav items colors wll change on scroll

--- Conference/assets/js/scroll.js
+++ Conference/assets/js/scroll.js
@@ -1,18 +1,24 @@
-
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "40px";
-    document.getElementById("navbar").style.backgroundColor = "#f1f1f1";
-  
+    document.getElementById("navbar").style.padding = "20px 5px";
+    document.getElementById("logo").style.fontSize = "30px";
+    document.getElementById("navbar").classList.remove('bg-transparent');
+    document.getElementById("navbar").style.backgroundColor = "#b3daff";
+    
+  document.getElementById("about").style.color = "#000";
+  document.getElementById("home").style.color = "#000";
+  document.getElementById("logo").style.color = "#000";
    
   } else {
-    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("navbar").style.padding = "60px 6px";
     document.getElementById("logo").style.fontSize = "50px";
-   document.getElementById("navbar").style.backgroundColor = '#f1f1f1';
-   
+   //document.getElementById("navbar").style.backgroundColor = '#000';
+      document.getElementById("navbar").classList.add('bg-transparent');
+   document.getElementById("about").style.color = "#fff";
+   document.getElementById("home").style.color = "#fff";
+   document.getElementById("logo").style.color = "#fff";
  
   }
 } 
